720d849aadd114f1d5d9de5a191065c08fca62c1
saroj-2728 <080bct075.saroj@pcampus.edu.np>
2025-06-01 01:51:11 +0545

    refactor: move Sanity config to env vars and fix Vercel build failure due to version conflict between react and next-sanity

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: "a8cg9mw6",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-01-01",
   useCdn: false,
 });
